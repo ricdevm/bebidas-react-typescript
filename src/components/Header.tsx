888e45daa8609e66ac51d669939715623ac607b2
RicDev <ricardo.aperez94@gmail.com>
2025-07-01 12:05:25 -0400

    API consumption of drinks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { NavLink, useLocation } from "react-router-dom"
+import { useAppStore } from "../stores/useAppStore"
 
 export default function Header() {
     const { pathname } = useLocation()
     const isHome = useMemo(() => pathname === "/" , [pathname])
+
+    const fetchCategories = useAppStore((state) => state.fetchCategories)
+
+    useEffect(() => {
+        fetchCategories()
+    })
 
     return (
         <>
--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,11 +1,20 @@
 import type { StateCreator } from "zustand"
-
-type Category = {}
+import { getCategories } from "../services/RecipeService"
+import type { Categories } from "../types"
 
 export type RecipesSliceType = {
-    categories: Category[]
+    categories: Categories
+    fetchCategories: () => Promise<void>
 }
 
-export const createRecipesSlice : StateCreator<RecipesSliceType> = () => ({
-    categories: []
+export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
+    categories: {
+        drinks: []
+    },
+    fetchCategories: async () => {
+        const categories = await getCategories()
+        set({
+            categories
+        })
+    }
 })
