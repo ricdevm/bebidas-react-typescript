661cafd6b713eb76b088c06aee76c241321ae205
RicDev <ricardo.aperez94@gmail.com>
2025-07-04 13:48:10 -0400

    add to favorites

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,6 +1,22 @@
-import { StateCreator } from "zustand";
+import type { StateCreator } from "zustand";
 import type { Recipe } from "../types";
 
 export type FavoritesSliceType = {
     favorites: Recipe[]
+    handleClickFavorite: (recipe: Recipe) => void
 }
+
+export const crateFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get) => ({
+    favorites: [],
+    handleClickFavorite: (recipe) => {
+        if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)) {
+            set((state) => ({
+                favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
+            }))
+        } else {
+            set((state) => ({
+                favorites: [...state.favorites, recipe]
+            }))
+        }
+    }
+})
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice";
+import { crateFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...crateFavoritesSlice(...a)
 })))
