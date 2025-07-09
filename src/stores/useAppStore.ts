b6bb1f44112d83de9fc251ecd03cf9fd8f935b78
RicDev <ricardo.aperez94@gmail.com>
2025-07-08 22:08:59 -0400

    the AI ​​slice is created

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,9 +3,11 @@ import { devtools } from "zustand/middleware";
 import { createRecipesSlice, type RecipesSliceType } from "./recipeSlice";
 import { crateFavoritesSlice, type FavoritesSliceType } from "./favoritesSlice";
 import { createNotificationSlice, type NotificationSliceType } from "./notificationSlice";
+import { createAISlice, type AISlice } from "./aiSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...crateFavoritesSlice(...a),
-    ...createNotificationSlice(...a)
+    ...createNotificationSlice(...a),
+    ...createAISlice(...a)
 })))
